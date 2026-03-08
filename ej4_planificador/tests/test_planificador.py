305a986b5f47f74b4871fd30869c274edc60e7e8
NicolasGutierrez2210 <nicolas.gutierrez06@usa.edu.co>
2026-03-08 11:41:28 -0500

    Add unit tests for Planificador class

--- ej4_planificador/tests/test_planificador.py
+++ ej4_planificador/tests/test_planificador.py
@@ -1,1 +1,34 @@
+import unittest
+from nucleo.gestor_cola import Tarea, Planificador
 
+
+class TestPlanificador(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.sistema = Planificador()
+
+    def test_agregar_y_ejecutar(self) -> None:
+        t1 = Tarea(3, 1, "proceso_A")
+        t2 = Tarea(1, 2, "proceso_B")
+
+        self.sistema.agregar(t1)
+        self.sistema.agregar(t2)
+
+        resultado = self.sistema.ejecutar()
+
+        self.assertEqual(resultado.descripcion, "proceso_B")
+
+    def test_misma_prioridad(self) -> None:
+        t1 = Tarea(1, 1, "proceso_A")
+        t2 = Tarea(1, 2, "proceso_B")
+
+        self.sistema.agregar(t1)
+        self.sistema.agregar(t2)
+
+        resultado = self.sistema.ejecutar()
+
+        self.assertEqual(resultado.descripcion, "proceso_A")
+
+
+if __name__ == "__main__":
+    unittest.main()
